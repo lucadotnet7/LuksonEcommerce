8f857e77ef86a953d948c718655876b09d8c44ee
lucadanielcanas7 <lucadanielcanas7@gmail.com>
2024-07-02 23:53:49 -0300

    Add automapper

---
+++ Ecommerce.WebApp/Business/Utils/Mappers/AutoMapperProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Ecommerce.WebApp.Model.DTOs;
+using Ecommerce.WebApp.Model.Entities;
+
+namespace Ecommerce.WebApp.Business.Utils.Mappers
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<Usuario, UserDto>();
+            CreateMap<Usuario, SessionDto>();
+            CreateMap<UserDto, Usuario>();
+
+            CreateMap<Categoria, CategoryDto>();
+            CreateMap<CategoryDto, Categoria>();
+
+            CreateMap<Producto, ProductDto>();
+            CreateMap<ProductDto, Producto>()
+                .ForMember(x => x.IdCategoriaNavigation, opt => opt.Ignore());
+        }
+    }
+}
